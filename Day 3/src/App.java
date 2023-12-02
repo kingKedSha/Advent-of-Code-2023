faceeabdae133b96a551817dcf64e69bceaea1dc
kingKedSha <107958821+kingKedSha@users.noreply.github.com>
2023-12-02 10:30:30 -0500

    prep for day 3

---
+++ Day 3/src/App.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+public class App {
+    public static void main(String[] args) throws FileNotFoundException {
+        System.out.println(part1());
+        // System.out.println(part2());
+    }
+
+    public static int part1() throws FileNotFoundException {
+        Scanner sc = new Scanner(new File("src/Input.txt"));
+        int total = 0;
+
+        while (sc.hasNextLine()) {
+            String s = sc.nextLine();
+        }
+
+        sc.close();
+        return 0;
+    }
+
+    public static int part2() throws FileNotFoundException {
+        Scanner sc = new Scanner(new File("src/Input.txt"));
+        int total = 0;
+
+        while (sc.hasNextLine()) {
+            String s = sc.nextLine();
+        }
+
+        sc.close();
+        return 0;
+    }
+}
