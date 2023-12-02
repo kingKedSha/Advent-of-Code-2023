4d6660103a22055df87e0a98a32b1e374af903ed
kingKedSha <107958821+kingKedSha@users.noreply.github.com>
2023-12-01 21:57:12 -0500

    SUBHI I WILL BEAT YOU

---
+++ Day 2/src/App.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class App {
+    public static void main(String[] args) throws FileNotFoundException {
+        System.out.println(part1());
+        System.out.println(part2());
+    }
+
+    public static String part1() throws FileNotFoundException {
+        Scanner sc = new Scanner(new File("src/Input.txt"));
+        sc.close();
+
+        return "haven't done this yet";
+    }
+
+    public static String part2() throws FileNotFoundException {
+        Scanner sc = new Scanner(new File("src/Input.txt"));
+        sc.close();
+
+        return "haven't done this yet";
+    }
+}
