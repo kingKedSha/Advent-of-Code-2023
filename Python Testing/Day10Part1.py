f89de01197e5ffe08d08567e3356d9d59d75bb7d
kingKedSha <107958821+kingKedSha@users.noreply.github.com>
2023-12-10 16:16:32 -0500

    forgot to commit part 2, made part 1 better

--- Python Testing/Day10Part1.py
+++ Python Testing/Day10Part1.py
@@ -6,39 +6,13 @@ for i, line in enumerate(lines):
 dir = "up"
 looplength = 0
 loopdone = False
-print(lines[ind[0]][ind[1]])
-print(lines)
+directions = {"right": (0, 1), "left": (0, -1), "up": (-1, 0), "down": (1, 0)}
+changes = {'F': {"up": "right", "left": "down"}, '7': {"up": "left", "right": "down"}, 'J': {"down": "left", "right": "up"}, 'L': {"down": "right", "left": "up"}}
 while loopdone == False:
-    if dir == "right":
-        ind[1] += 1
-    elif dir == "left":
-        ind[1] -= 1
-    elif dir == "up":
-        ind[0] -= 1
-    elif dir == "down":
-        ind[0] += 1
-    if lines[ind[0]][ind[1]] == 'F':
-        if dir == "up":
-            dir = "right"
-        else:
-            dir = "down"
-    elif lines[ind[0]][ind[1]] == "7":
-        if dir == "up":
-            dir = "left"
-        else:
-            dir = "down"
-    elif lines[ind[0]][ind[1]] == "J":
-        if dir == "down":
-            dir = "left"
-        else:
-            dir = "up"
-    elif lines[ind[0]][ind[1]] == "L":
-        if dir == "down":
-            dir = "right"
-        else:
-            dir = "up"
-    looplength+=1
-    if lines[ind[0]][ind[1]] == 'S':
-        loopdone = True
-    print(dir)
-print(looplength/2)
+    ind[0] += directions[dir][0]
+    ind[1] += directions[dir][1]
+    if lines[ind[0]][ind[1]] in changes:
+        dir = changes[lines[ind[0]][ind[1]]][dir]
+    looplength += 1
+    loopdone = lines[ind[0]][ind[1]] == 'S'
+print(int(looplength/2))
